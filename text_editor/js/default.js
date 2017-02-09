1b88b0f0a85b63e07051d834cb821691b39eb509
clucle <wjdenwls123@gmail.com>
2017-02-09 15:56:07 +0900

    feat: indent wrapping
    안띄어썻을때 숫자 추가안되고
    
    엔터처야만 숫자 추가

--- text_editor/js/default.js
+++ text_editor/js/default.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
 	var req_code = $(".codemirror-textarea")[0];
 	var req_editor = CodeMirror.fromTextArea(req_code, {
-		lineNumbers: true
+		lineNumbers: true,
+		lineWrapping: true
 	});
 
 	var res_code = $(".codemirror-textarea")[1];
 	var res_editor = CodeMirror.fromTextArea(res_code, {
-		lineNumbers: true
+		lineNumbers: true,
+		lineWrapping: true
 	});
 });
