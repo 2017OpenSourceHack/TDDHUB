fbc146fa067c815d72f4f7a8afbe0c3d578c8830
Taehoon Seo <shpongle2634@naver.com>
2017-02-09 18:57:19 +0900

    UI feat: add Category, add Testcase. API Test, Test OK, Success Status

--- public/project/detail/controller.js
+++ public/project/detail/controller.js
@@ -4,8 +4,9 @@ app.controller('detail', function($scope, $http,$state,$stateParams, toastr){
 //   $scope.data = { selectedSid : ''}
   $scope.project={};
   $scope.category={};
+  $scope.newcategory={};
+  $scope.newtestcase={};
   $scope.testcase={};
-  $scope.height=37;
 //
 //
   $scope.view = function () {
@@ -19,58 +20,75 @@ app.controller('detail', function($scope, $http,$state,$stateParams, toastr){
 
   $scope.selectCategory = function(item){
     $scope.category=item;
+    $scope.testcase={};
   };
 
-  $scope.selectTestcase = function(item){
-    $scope.testcase=item;
+  $scope.selectTestcase = function(item,subitem){
+    $scope.category=item;
+    $scope.testcase=subitem;
+    $scope.newtestcase=subitem;
+    console.log(  $scope.category);
+    console.log(  $scope.testcase);
   };
 
   $scope.test = function (item) {
+      if(!item.method || !item.url ||!$scope.category){
+        toastr.error('Wrong Input.');
+        return;
+      }
       console.log(item);
+
       if (item.method === 'post' ||item.method === 'POST') {
-          $http.post( $scope.project.serverurl+item.url, item.data).then(function (result) {
+          $http.post( $scope.project.serverurl+item.url, item.request).then(function (result) {
               toastr.success('Success');
-              item.response=result;
+              item.response=JSON.stringify(result.data);
+              $http.post('/projects/'+$stateParams.id+'/categories/'+$scope.category.sid+'/testcases', item);
           }, function (error) {
               toastr.error('Error');
-              item.response=error;
-          })
+              item.response=JSON.stringify(error);
+          });
       } else if (item.method === 'put' ||item.method === 'PUT') {
-          $http.put($scope.project.serverurl+item.url, item.data).then(function (result) {
+          $http.put($scope.project.serverurl+item.url, item.request).then(function (result) {
               toastr.success('Success');
-              item.response=result;
+              item.response=JSON.stringify(result.data);
+              $http.post('/projects/'+$stateParams.id+'/categories/'+$scope.category.sid+'/testcases', item);
           }, function (error) {
               toastr.error('Error');
-              item.response=error;
-          })
+              item.response=JSON.stringify(error);
+          });
       } else if (item.method === 'delete' || item.method === 'DELETE') {
           $http.delete($scope.project.serverurl+item.url).then(function (result) {
               toastr.success('Success');
-              item.response=result;
+              item.response=JSON.stringify(result.data);
+              $http.post('/projects/'+$stateParams.id+'/categories/'+$scope.category.sid+'/testcases', item);
           }, function (error) {
               toastr.error('Error');
-              item.response=error;
-          })
+              item.response=JSON.stringify(error);
+          });
       } else if (item.method === 'get' ||item.method === 'GET') {
           $http.get($scope.project.serverurl+item.url).then(function (result) {
               toastr.success('Success');
-                item.response=result;
+                item.response=JSON.stringify(result.data);
+                $http.post('/projects/'+$stateParams.id+'/categories/'+$scope.category.sid+'/testcases', item);
           }, function (error) {
               toastr.error('Error');
-              item.response=error;
-          })
+              item.response=JSON.stringify(error);
+          });
       }
 
-      $http.post('/projects/'+$scope.project.sid+'/categories/'+$scope.category.sid+'/testcases', item);
+
 
   }
 
-//   $scope.new_category = function (item) {
-//     $http.post('/users/'+$scope.user.sid+'/projects').then(function () {
-//       toastr.success("생성완료.");
-// $scope.view()
-//     })
-//   }
+  $scope.new_category = function (item) {
+    console.log(item);
+    $http.post('/projects/'+$stateParams.id+'/categories',item).then(function (result) {
+      toastr.success("Completed.");
+      $scope.view();
+    }, function(error){
+      toastr.error('Error');
+    });
+  };
 //
 //   $scope.delete_category = function () {
 //     $http.post('/projects/'+$scope.data.selectedSid+'/users?email='+$scope.useremail).then(function () {
@@ -78,11 +96,20 @@ app.controller('detail', function($scope, $http,$state,$stateParams, toastr){
 //     })
 //   }
 //
-//   $scope.new_testcase = function (item) {
-//     $http.get('/users/'+$scope.user.sid+'/projects/'+item.sid).then(function () {
-// $scope.view()
-//     })
-//   }
+  $scope.new_testcase = function (item) {
+    if(!item.name){
+      toastr.error('Enter the API Description.');
+      return;
+    }
+    item.method="GET";
+    item.url="/";
+    $http.post('/projects/'+$stateParams.id+'/categories/'+$scope.category.sid+'/testcases',item).then(function () {
+      toastr.success('Completed.');
+      $state.reload();
+    }, function(error){
+      toastr.error('Error');
+    });
+  };
 //
 //     $scope.delete_testcase = function () {
 //       $http.post('/users/'+item.sid+'/projects').then(function () {
@@ -90,5 +117,14 @@ app.controller('detail', function($scope, $http,$state,$stateParams, toastr){
 //       })
 //     }
 
+$scope.done=function(){
+  $http.put('/projects/'+$stateParams.id+'/categories/'+$scope.category.sid+'/testcases/'+$scope.testcase.sid+'/users/'+$scope.user.sid).then(function(){
+    toastr.success('Test Done Applied.');
+    $scope.view();
+  }, function(error){
+    toastr.error('Error');
+  });
+
+};
 
 });
